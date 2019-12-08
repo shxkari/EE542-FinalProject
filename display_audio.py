e0f04b11bbe133f15f2e4527007eaec6be9e9f7d
Yutong Gu <gyutong@gmil.com>
2019-12-07 17:05:31 -0800

    updated CNN.pickle for python3, fixed some logic errors in record-sound and wrote a script to display the recorded library

---
+++ display_audio.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pydub import AudioSegment
+import sys
+
+import librosa.display
+
+max_pad_len = 174
+
+def extract_features(data, fs):
+    mfccs = librosa.feature.mfcc(y=data, sr=fs, n_mfcc=40)
+    print(mfccs.shape)
+    pad_width = max_pad_len - mfccs.shape[1]
+    mfccs = np.pad(mfccs, pad_width=((0, 0), (0, pad_width)), mode='constant')
+    return mfccs
+
+audio = AudioSegment.from_wav("output.wav", )
+frame_count = audio.frame_count()
+frame_rate = audio.frame_rate
+samples = audio.get_array_of_samples()
+
+print("Number of channels: " + str(audio.channels))
+print("Frame count: " + str(frame_count))
+print("Frame rate: " + str(frame_rate))
+print("Sample width: " + str(audio.sample_width))
+print("Number of samples: " + str(len(samples)))
+
+"""***********************FULL SAMPLE PLOT**************************"""
+period = 1/frame_rate                     #the period of each sample
+duration = frame_count/frame_rate         #length of full audio in seconds
+time = np.arange(0, duration, period)     #generate a array of time values from 0 to [duraction] with step of [period]
+
+#TODO: Plot the full sample as a subplot
+plt.figure(figsize=(20,10))
+plt.subplot(1,2,1)
+plt.plot(time, samples)
+plt.title('Waveform')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+
+myrecording, sample_rate = librosa.load("output.wav", res_type='kaiser_fast') 
+feat = extract_features(myrecording, frame_rate)
+plt.subplot(1,2,2)
+librosa.display.specshow(feat, x_axis='time', )
+plt.colorbar()
+plt.title('MFCC')
+plt.tight_layout()
+plt.show()
--- record-sound.py
+++ record-sound.py
@@ -1,5 +1,6 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
+import wave
 import pickle
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
@@ -7,43 +8,42 @@ from keras.utils import to_categorical
 import librosa
 import numpy as np
 
-fs = 44100  # Sample rate
+fs = 22050  # Sample rate
 seconds = 3  # Duration of recording
 
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
-
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype=np.int16)
+print("RECORDING AUDIO")
 print(myrecording.dtype)
 
 print(myrecording)
 sd.wait()  # Wait until recording is finished
+print("DONE")
 write('output.wav', fs, myrecording)  # Save as WAV file 
+
+file_name = "output.wav"
+myrecording, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
+print(myrecording)
+
+# write('output.wav', fs, myrecording)  # Save as WAV file 
 
 # unpickling the pickles
 loaded_model = pickle.load(open('CNN_model.pickle', 'rb'))
-le = pickle.load(open('le.pickle', 'rb'))
+le = pickle.load(open('LE.pickle', 'rb'))
 
 num_rows = 40
 num_columns = 174
 num_channels = 1
 max_pad_len = 174
 
-def extract_features(file_name):
-    try:
-        audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
-        mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
-        pad_width = max_pad_len - mfccs.shape[1]
-        mfccs = np.pad(mfccs, pad_width=((0, 0), (0, pad_width)), mode='constant')
-        
-    except Exception as e:
-        print("Error encountered while parsing file: ", file_name)
-        print("--------------")
-        print(e)
-        print("--------------")
-        return None 
+def extract_features(data):
+    mfccs = librosa.feature.mfcc(y=data, sr=fs, n_mfcc=40)
+    print(mfccs.shape)
+    pad_width = max_pad_len - mfccs.shape[1]
+    mfccs = np.pad(mfccs, pad_width=((0, 0), (0, pad_width)), mode='constant')
     return mfccs
 
-def print_prediction(file_name):
-    prediction_feature = extract_features(file_name) 
+def print_prediction(data):
+    prediction_feature = extract_features(data) 
     #print(prediction_feature)
     prediction_feature = prediction_feature.reshape(1, num_rows, num_columns, num_channels)
     #print(prediction_feature)
@@ -57,21 +57,8 @@ def print_prediction(file_name):
         category = le.inverse_transform(np.array([i]))
         print(category[0], "\t\t : ", format(predicted_proba[i], '.32f') )
 
-fname = 'output.wav'
+print_prediction(myrecording)
 
-#print_prediction(fname)
-
-print(('The predicted class is:', 'gun_shot', '\n'))
-print('air_conditioner', '\t\t : ', '0.00000130929652186750900000333786')
-print('car_horn', '\t\t : ', '0.00001913116102514322847127914429')
-print('children_playing', '\t\t : ', '0.00365575496107339859008789062500')
-print('dog_bark', '\t\t : ', '0.06901239603757858276367187500000')
-print('drilling', '\t\t : ', '0.00368810654617846012115478515625')
-print('engine_idling', '\t\t : ', '0.00033480214187875390052795410156')
-print('gun_shot', '\t\t : ', '0.92230415344238281250000000000000')
-print('jackhammer', '\t\t : ', '0.00000770927272242261096835136414')
-print('siren', '\t\t : ', '0.00055570778204128146171569824219')
-print('street_music', '\t\t : ', '0.00042099214624613523483276367188')
 
 
 
