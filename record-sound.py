eba4c047b32ce3a0d8b47bfc107b83e240e66cb8
Yutong Gu <gyutong@gmil.com>
2019-12-09 22:57:50 -0800

    updated record sound and display audio, and the jupyter notebooks

--- record-sound.py
+++ record-sound.py
@@ -7,9 +7,9 @@ from sklearn.preprocessing import LabelEncoder
 from keras.utils import to_categorical
 import librosa
 import numpy as np
-
+import time
 fs = 22050  # Sample rate
-seconds = 3  # Duration of recording
+seconds = 2  # Duration of recording
 
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1, dtype=np.int16)
 print("RECORDING AUDIO")
@@ -36,17 +36,21 @@ num_channels = 1
 max_pad_len = 174
 
 def extract_features(data):
-    mfccs = librosa.feature.mfcc(y=data, sr=fs, n_mfcc=40)
-    print(mfccs.shape)
+    mfccs = librosa.feature.mfcc(y=data, sr=fs, n_mfcc=num_rows, hop_length=512)
     pad_width = max_pad_len - mfccs.shape[1]
-    mfccs = np.pad(mfccs, pad_width=((0, 0), (0, pad_width)), mode='constant')
+    if(pad_width < 0):
+        print(file_name)
+        mfccs = mfccs[:,:pad_width].copy()
+    else:
+        mfccs = np.pad(mfccs, pad_width=((0, 0), (0, pad_width)), mode='constant')
     return mfccs
 
 def print_prediction(data):
     prediction_feature = extract_features(data) 
     #print(prediction_feature)
     prediction_feature = prediction_feature.reshape(1, num_rows, num_columns, num_channels)
-    print(prediction_feature.shape)
+    #print(prediction_feature)
+    time_now = time.time()
     predicted_vector = loaded_model.predict_classes(prediction_feature)
     predicted_class = le.inverse_transform(predicted_vector) 
     print("The predicted class is:", predicted_class[0], '\n') 
@@ -56,7 +60,7 @@ def print_prediction(data):
     for i in range(len(predicted_proba)): 
         category = le.inverse_transform(np.array([i]))
         print(category[0], "\t\t : ", format(predicted_proba[i], '.32f') )
-
+    print("Prediction took {} seconds".format(time.time() - time_now))
 print_prediction(myrecording)
 
 
